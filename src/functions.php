c161e0c6f970f2556897d56d2298720989a8e1b2
alpipego <alpipego@gmail.com>
2017-07-22 18:00:32 +0200

    add object() function

---
+++ src/functions.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: alpipego
+ * Date: 22.07.2017
+ * Time: 17:55
+ */
+
+namespace WPHibou\DI;
+
+function object()
+{
+    return new ObjectDefinition();
+}
